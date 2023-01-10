0e11534b11cb95d920b086da9a96cfe313d393c5
Murat <murtum07@gmail.com>
2023-01-10 12:48:15 +0300

    init

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -59,3 +59,25 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
+    
+    
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Находит выбросы в данных, используя квантили. 
+    Исключает по 1% распределения по обе стороны.
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): граница квантили в левую сторону распределения. По умолчанию 0.01.
+        right (float, optional): граница квантили в правую сторону распределения. По умолчанию 0.99.
+        
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
